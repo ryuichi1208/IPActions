b5d99b9c7997612f92d5facabc2177401e4d477c
ryuichi1208 <ryucrosskey@gmail.com>
2019-10-17 22:47:44 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Actions
+
+![logs](https://github.com/ryuichi1208/Actions/image/actions_log.png)
---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(
+    name="Actions",
+    version="1.0",
+    description="A package that implements various actions.",
+    author="ryuichi1208",_
+    author_email="ryucrosskey@gmail.com",
+    url="",
+    license="",
+)
